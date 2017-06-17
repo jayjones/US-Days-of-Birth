720c1d059b55512f5d5807c840f8aca24a7d5ae3
Jeff Jones <jonesjp6@gmail.com>
2017-06-17 16:38:00 -0500

    Bar
    Adding a Bar chart with MatPlotLib.PyPlot

--- US Days of Births.py
+++ US Days of Births.py
@@ -10,4 +10,10 @@ for i in data:
     else:
         day_counts[str(day[3])] = int(day[4])
 
+import matplotlib.pyplot as plt
+
 print(day_counts)
+plt.bar(range(len(day_counts)), day_counts.values())
+plt.xticks(range(len(day_counts)), ("Saturday", "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"), rotation=45)
+
+plt.show()
